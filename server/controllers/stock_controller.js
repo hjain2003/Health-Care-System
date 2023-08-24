0445a9556f6ae797b1905cd6fdf00e36847f9f3f
Harsh <hjain_be21@thapar.edu>
2023-08-24 20:30:51 +0530

    fixed few issues in booking controller

---
+++ server/controllers/stock_controller.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import Stock from '../models/Stock.js'; 
+
+export const stockAdd = async (req, res) => {
+    try {
+        const { medicine, price, quantity } = req.body;
+
+        if (!medicine || !price || !quantity) {
+            return res.status(422).json({ error: 'empty fields!' });
+        }
+
+        const stockEntry = new Stock({
+            medicine,
+            price,
+            quantity,
+        });
+
+        await stockEntry.save();
+
+        return res.status(201).json({ message: 'Stock information added successfully' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'An error occurred' });
+    }
+};
+
+export const viewAllStocks = async(req,res)=>{
+    try {
+        const stocks = await Stock.find();
+
+        if (!stocks) {
+            return res.status(404).json({ message: 'No items found' });
+        }
+
+        return res.status(200).json({ stocks });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'An error occurred' });
+    }
+}
+
+export const deleteStockItem = async(req,res)=>{
+    try {
+        const stockId = req.params.stockId;
+
+        const stockItem = await Stock.findById(stockId);
+
+        if (!stockItem) {
+            return res.status(404).json({ error: 'Stock item not found' });
+        }
+
+        await Stock.findByIdAndDelete(stockId);
+
+        return res.status(200).json({ message: 'Stock item deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'An error occurred' });
+    }
+}
+
+
+
+
+
+
--- server/routes/stock_routes.js
+++ server/routes/stock_routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { deleteStockItem, stockAdd, viewAllStocks } from '../controllers/stock_controller.js';
 
 const stockRouter = express.Router();
 
-stockRouter.put('/editCount',);
-stockRouter.post('/addInfo',);
-stockRouter.get('/viewStock',);
+stockRouter.put('/incrementCount',);
+stockRouter.put('/decrementCount',)
+stockRouter.post('/addInfo',stockAdd);
+stockRouter.get('/viewStock',viewAllStocks);
+stockRouter.delete('/delStock/:stockId',deleteStockItem);
 
 export default stockRouter;
