079547d6e1956b39d321e1a0ef4af9f29c1b57ce
Harsh <hjain_be21@thapar.edu>
2023-08-26 15:35:39 +0530

    added register/login

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import './Login.css';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Login = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="full_page_login">
+        <div className="login_box">
+          <h2 align="center">LOGIN</h2>
+          <label htmlFor="">Email ID</label>
+          <input type="text" placeholder='Enter Email-id' name="email" />
+          <br />
+          <label htmlFor="">Password</label>
+          <div className="password-input">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder='Enter password'
+              name="password"
+              className="password_field"
+            />
+            <button className="password-toggle-btn" onClick={togglePasswordVisibility}>
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
+          </div>
+          <br /><br />
+          <button className='login_btn'>SIGN IN</button>
+          <br />
+          <span className="linkRegister">Already have an account ? <NavLink to='/register'>Register here!</NavLink></span>
+        </div>
+      </div >
+    </>
   )
 }
 
--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -1,27 +1,66 @@
-import React from 'react'
+import React from 'react';
 import './Register.css';
+import { NavLink } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Register = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
   return (
     <>
-    <div className="full_page_register">
+      <div className="full_page_register">
         <div className="register_box">
-            <h2 align="center">REGISTER</h2>
-            <label htmlFor="">Name</label>
-            <input type="text" placeholder='Enter Name' name="name" />
-            <br />
-            <label htmlFor="">Email ID</label>
-            <input type="email" placeholder='Enter email-id' name="email" />
-            <br />
-            <label htmlFor="">Password</label>
-            <input type="password" placeholder='Enter password' name="password" />
-            <br />
-            <label htmlFor="">Confirm Password</label>
-            <input type="password" placeholder='Re-Enter password' name="cpassword" />
+          <h2 align="center">REGISTER</h2>
+          <label htmlFor="">Name</label>
+          <input type="text" placeholder='Enter Name' name="name" />
+          <br />
+          <label htmlFor="">Email ID</label>
+          <input type="email" placeholder='Enter email-id' name="email" />
+          <br />
+          <label htmlFor="">Password</label>
+          <div className="password-input">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder='Enter password'
+              name="password"
+              className="password_field"
+            />
+            <button className="password-toggle-btn" onClick={togglePasswordVisibility}>
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
+          </div>
+          <br />
+          <label htmlFor="">Confirm Password</label>
+          <div className="password-input">
+            <input
+              type={showConfirmPassword ? 'text' : 'password'}
+              placeholder='Re-Enter password'
+              name="cpassword"
+              className="password_field"
+            />
+            <button className="password-toggle-btn" onClick={toggleConfirmPasswordVisibility}>
+              {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
+          </div>
+          <br />
+          <button className='register_btn'>SIGN UP</button>
+          <br />
+          <span className="linkRegister">
+            Already have an account ? <NavLink to='/login'>Login here!</NavLink>
+          </span>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Register
+export default Register;
